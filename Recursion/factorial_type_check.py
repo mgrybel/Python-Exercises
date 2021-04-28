da68db34b5dac79ccd94e68771b0c7d684498d29
mgrybel <monika.grybel@hotmail.com>
2021-04-28 23:19:07 +0100

    Update

---
+++ Recursion/factorial_type_check.py
@@ -0,0 +1,15 @@
+def factorial(n):
+    if not isinstance(n, int):
+        print('Factorial is only defined for integers.')
+        return None
+    elif n < 0:
+        print('Factorial is not defined for negative integers.')
+        return None
+    elif n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+
+factorial(1.5)
+factorial(-5)
