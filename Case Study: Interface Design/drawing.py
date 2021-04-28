32bba696b4be7008d4d2670d429c764c473105da
mgrybel <monika.grybel@hotmail.com>
2021-04-28 21:36:02 +0100

    Update

--- Case Study: Interface Design/drawing.py
+++ Case Study: Interface Design/drawing.py
@@ -10,6 +10,10 @@ def square(t, length):
         t.lt(90)
 
 def polyline(t, n, length, angle):
+    '''
+    Draws n line segments with the given length and angle (in degrees)
+    between them. t is a turtle.
+    '''
     for i in range(n):
         t.fd(length)
         t.lt(angle)
