2cc4949850babf15aab7019dff1dd1fd0d133811
mgrybel <monika.grybel@hotmail.com>
2021-04-28 21:26:32 +0100

    Update

--- Case Study: Interface Design/mypolygon.py
+++ Case Study: Interface Design/mypolygon.py
@@ -1,4 +1,11 @@
+import math
 import turtle
+
+def circle(t, r):
+    circumference = 2 * math.pi * r
+    n = int(circumference / 3) + 1
+    length = circumference / n
+    polygon(t, n, length)
 
 def square(t, length):
     for i in range(4):
@@ -12,9 +19,6 @@ def polygon(t, n, length):
         t.lt(angle)
 
 bob = turtle.Turtle()
-polygon(bob, n=7, length=70)
-
-alice = turtle.Turtle()
-square(alice, length=100)
+circle(bob, r=100)
 
 turtle.mainloop()
