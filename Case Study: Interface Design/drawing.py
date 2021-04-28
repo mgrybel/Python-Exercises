8cf0ca695f9cbd87396154dc87ffeb1f9339dee6
mgrybel <monika.grybel@hotmail.com>
2021-04-28 21:31:56 +0100

    Update

---
+++ Case Study: Interface Design/drawing.py
@@ -0,0 +1,31 @@
+import math
+import turtle
+
+def circle(t, r):
+    arc(t, r, 360)
+
+def square(t, length):
+    for i in range(4):
+        t.fd(length)
+        t.lt(90)
+
+def polyline(t, n, length, angle):
+    for i in range(n):
+        t.fd(length)
+        t.lt(angle)
+
+def polygon(t, n, length):
+    angle = 360 / n
+    polyline(t, n, length, angle)
+
+def arc(t, r, angle):
+    arc_length = 2 * math.pi * r  * angle / 360
+    n = int(arc_length / 3) + 1
+    step_length = arc_length / n
+    step_angle = float(angle) / n
+    polyline(t, n, step_length, step_angle)
+
+bob = turtle.Turtle()
+circle(bob, r=100)
+
+turtle.mainloop()
