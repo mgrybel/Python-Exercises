c685a517d24ddc1417c403faf3a212ab87c883f3
mgrybel <monika.grybel@hotmail.com>
2021-04-28 21:04:17 +0100

    Update

--- Case Study: Interface Design/mypolygon.py
+++ Case Study: Interface Design/mypolygon.py
@@ -1,16 +1,10 @@
 import turtle
 
 bob = turtle.Turtle()
-bob.fd(100)
-bob.lt(90)
 
-bob.fd(100)
-bob.lt(90)
-
-bob.fd(100)
-bob.lt(90)
-
-bob.fd(100)
+for i in range(4):
+    bob.fd(100)
+    bob.lt(90)
 
 print(bob)
 
