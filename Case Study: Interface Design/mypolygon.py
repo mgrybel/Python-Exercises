fd78072af759ea6e21b29955cec51cf2d9ab18ea
mgrybel <monika.grybel@hotmail.com>
2021-04-28 21:08:55 +0100

    Update

--- Case Study: Interface Design/mypolygon.py
+++ Case Study: Interface Design/mypolygon.py
@@ -1,11 +1,12 @@
 import turtle
 
+def square(t):
+    for i in range(4):
+        t.fd(100)
+        t.lt(90)
+
+
 bob = turtle.Turtle()
-
-for i in range(4):
-    bob.fd(100)
-    bob.lt(90)
-
-print(bob)
+square(bob)
 
 turtle.mainloop()
