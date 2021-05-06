ee6f334a7cf51ad3986312178b0d21f6bf9bfb5d
mgrybel <monika.grybel@hotmail.com>
2021-05-06 16:39:27 +0100

    Update

--- Think-Python/Files/checksum.py
+++ Think-Python/Files/checksum.py
@@ -2,7 +2,7 @@ import os
 
 filename = '/Users/monika/Desktop/GitHub/Python-Exercises/Think-Python/Case_Study_Data_Structure_Selection/Dracula.txt'
 
-cmd = 'md5sum' + filename
+cmd = 'md5sum ' + filename
 
 fp = os.popen(cmd)
 res = fp.read()
